0c9869b53aac317087e7434083c9279ae12896ca
Matt F <fiaschettima@gmail.com>
2022-07-30 19:11:04 -0400

    product destroy route updated

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -108,8 +108,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try{
+    const deletedProduct = await Product.destroy(
+      {
+        where: {id: req.params.id},
+      }
+    );
+    if(!deletedProduct){
+      res.status(404).json({message:'No Product found with given Id'})
+      return
+    };
+    res.status(200).json(deletedProduct)
+  }catch(err){
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
