e07feb569d1f9d174b88e8184f6bb0a4e8b0b698
Matt F <fiaschettima@gmail.com>
2022-07-30 17:43:53 -0400

    route to create new category updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,18 +11,29 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+// To Fix Later Sequelize statment is showing category Id twice in products,
+// sql statment causing this needed to be removed is `products`.`category_id` AS `products.category_id`,
 router.get('/:id', async (req, res) => {
   try{
-    const categoryData = await Category.findByPk(req.params.id)
+    const categoryData = await Category.findByPk(req.params.id,{
+      include: [{model: Product} ]
+    });
+    if(!categoryData){
+      res.status(404).json({message: 'No matching Category Found'})
+    };
     res.status(200).json(categoryData);
   }catch(err){
     res.status(500).json(err)
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try{
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  }catch(err){
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
