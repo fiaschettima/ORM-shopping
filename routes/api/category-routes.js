e74c710bfc584d94e0729b30e6e7aeb43ef7b1ef
Matt F <fiaschettima@gmail.com>
2022-07-30 17:02:18 -0400

    get category by id route works

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,8 +15,9 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try{
     const categoryData = await Category.findByPk(req.params.id)
+    res.status(200).json(categoryData);
   }catch(err){
-
+    res.status(500).json(err)
   }
 });
 
