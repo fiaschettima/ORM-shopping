66189aca8527efd419c148d0249bc43a66b77407
Matt F <fiaschettima@gmail.com>
2022-07-31 18:53:51 -0400

    tag delete and update route working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -50,11 +50,23 @@ router.put('/:id', async (req, res) => {
 }catch(err){
   res.status(500).json(err);
 }
-  // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try{
+    const tagData = await Tag.destroy({
+      where:{
+        id: req.params.id
+      }
+    });
+    if(!tagData){
+      res.status(404).json({message: 'No Tag Found With that Id'})
+      return
+    }
+    res.status(200).json(tagData)
+  }catch(err){
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
